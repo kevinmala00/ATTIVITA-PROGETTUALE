0e69275ba1359e3a3ff874359b164e781ee0ad55
kevinmala00 <64544191+kevinmala00@users.noreply.github.com>
2022-05-18 16:31:16 +0200

    add radiobutton control and clicklistener in ButtonsActivity.java

--- Code/TheoryApp/AndroidWidgetToolboxApp/app/src/main/java/com/example/androidwidgettoolboxapp/ButtonsActivity.java
+++ Code/TheoryApp/AndroidWidgetToolboxApp/app/src/main/java/com/example/androidwidgettoolboxapp/ButtonsActivity.java
@@ -3,12 +3,35 @@ package com.example.androidwidgettoolboxapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class ButtonsActivity extends AppCompatActivity {
 
+    TextView tvResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buttons);
+        tvResult=findViewById(R.id.tv_result);
+
+        RadioButton rbJavaLanguage =findViewById(R.id.rb_java_language);
+        RadioButton rbCLanguage=findViewById(R.id.rb_c_language);
+
+        rbCLanguage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvResult.setVisibility(View.VISIBLE);
+                tvResult.setText(rbCLanguage.getText());
+            }
+        });
+        rbJavaLanguage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvResult.setVisibility(View.VISIBLE);
+                tvResult.setText(rbJavaLanguage.getText());
+            }
+        });
     }
 }
